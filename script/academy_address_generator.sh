c34f986475e3db05b73a15cf4b37af6447c8b34e
edusqaud <edusquad@gmail.com>
2017-12-31 17:34:31 +0900

    구분자를 변수로 따로 뺌, 근거데이터에 식별자를 둠에 따른 변경

--- script/academy_address_generator.sh
+++ script/academy_address_generator.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-## Kakao local api 
-
-
-KAKAO_URL=https://dapi.kakao.com/v2/local/search/address.json
-APP_KEY="Put in Your Kakao APP KEY"
+APP_KEY="Put in Your Daum APP KEY"
 CSV_FILE="Put in Your Address CSV File Path"
-OUTCOME_FILE=outfile.csv
+
+COMPARATOR="|"
+
 ## CSV File Format
 ##학원명,학원주소
 
-## First of all, You should : `apt install jq`
-
-touch ${OUTCOME_FILE}
+touch outfile.csv
 while read line
 do
-        NAME=`echo $line | awk -F[","] {'print $1'} |sed -e "s/\"//g"`
-        QUERY_ADDRESS=`echo $line | awk -F[","] {'print $2'} |sed -e "s/\"//g"`
-	curl -v -X GET "${KAKAO_URL}" --data-urlencode "query=${QUERY_ADDRESS}" -H "Authorization: KakaoAK ${APP_KEY}" | jq '.'> test
+	INDEX=`echo $line | awk -F[$COMPARATOR] {'print $1'}`
+        NAME=`echo $line | awk -F[$COMPARATOR] {'print $2'} |sed -e "s/\"//g"`
+        QUERY_ADDRESS=`echo $line | awk -F[$COMPARATOR] {'print $3'} |sed -e "s/\"//g"`
+	curl -v -X GET "https://dapi.kakao.com/v2/local/search/address.json" --data-urlencode "query=${QUERY_ADDRESS}" -H "Authorization: KakaoAK ${APP_KEY}" | jq '.'> test
 	
-	echo "${NAME},`cat test | jq '.documents[0] |{road_address: .road_address.address_name,building_name: .road_address.building_name, address_name: .address.address_name, region_1depth_name: .address.region_1depth_name, region_2depth_name: .address.region_2depth_name, region_3depth_h_name: .address.region_3depth_h_name, zip_code: .address.zip_code, x: .x, y: .y }'  | grep "road_address\|building_name\|address_name\|region_1depth_name\|region_2depth_name\|region_3depth_h_name\|zip_code\|\"x\"\|\"y\"" |sed -e "s/  //g"| awk -F[":"] {'print $2'}|tr "\n" "|"| sed -e "s/|//g" | sed -e "s/,$//g" |sed -e "s/\\"//g" `" >> ${OUTCOME_FILE}
+	echo "${INDEX},${NAME},`cat test | jq '.documents[0] |{road_address: .road_address.address_name,building_name: .road_address.building_name, address_name: .address.address_name, region_1depth_name: .address.region_1depth_name, region_2depth_name: .address.region_2depth_name, region_3depth_h_name: .address.region_3depth_h_name, zip_code: .address.zip_code, x: .x, y: .y }'  | grep "road_address\|building_name\|address_name\|region_1depth_name\|region_2depth_name\|region_3depth_h_name\|zip_code\|\"x\"\|\"y\"" |sed -e "s/  //g"| awk -F[":"] {'print $2'}|tr "\n" "|"| sed -e "s/|//g" | sed -e "s/,$//g" |sed -e "s/\\"//g" `" >> outfile.csv
 done < $CSV_FILE
 
 rm -rf test
